b5acea2946bb50ec8a65aaaf6f86629e78b8c00a
Eddy <eddy@qdata-mg.com>
2022-10-11 10:58:47 +0300

    enlever symfony 5

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-// ./src/js/app.js
-    
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import '../css/app.css';
-import Home from './components/Home';
-    
-ReactDOM.render(<Router><Home /></Router>, document.getElementById('root'));
